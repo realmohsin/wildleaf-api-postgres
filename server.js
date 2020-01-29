9c360580296f35f523c859f69a0115397ee4235f
Real Mohsin <realmohsin@outlook.com>
2020-01-29 07:38:18 -0500

    server creation

--- server.js
+++ server.js
@@ -5,3 +5,25 @@ process.on('uncaughtException', err => {
   logError(err)
   process.exit(1)
 })
+
+const express = require('express')
+
+const app = express()
+
+app.route('/').get((req, res) => {
+  res.send('🍂 wildleaf-api-postgres')
+})
+
+app.route('*').all((req, res) => {
+  res.send('💥 404 💥')
+})
+
+const server = app.listen('3000', () =>
+  log('🔋 Server running on port 3000... 🔋')
+)
+
+process.on('unhandledRejection', err => {
+  logError('Unhandled Rejection 💥 Shutting down...')
+  logError(err)
+  server.close(() => process.exit(1))
+})
