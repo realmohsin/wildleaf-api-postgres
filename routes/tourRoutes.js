eeaa6e5275fcaaea3655c74ad14fc233481a2511
Real Mohsin <realmohsin@outlook.com>
2020-01-29 19:28:06 -0500

    tour routes

---
+++ routes/tourRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const router = express.Router()
+
+router
+  .route('/api/v1/tours')
+  .get()
+  .post()
+
+router
+  .route('/api/v1/tours/:id')
+  .get()
+  .patch()
+  .delete()
+
+router.route('/tour-stats').get()
+
+router.route('/monthly-tour-starts/:year').get()
+
+router.route('/top-5-cheap').get()
